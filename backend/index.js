2d85d47395df4044bc65a7e90351f62d2191a251
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-04-24 11:00:45 +0530

    Upadated Index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const app = express();
-
 require("dotenv").config;
-const port = process.env.PORT;
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
