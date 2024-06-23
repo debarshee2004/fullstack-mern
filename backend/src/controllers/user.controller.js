f1e8913dc9ed4aa0badf1177ab120569e788c1ae
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-06-23 19:39:01 +0530

    wrote watch history controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -485,7 +485,7 @@ const getUserChannelProfile = AsyncFnHandler(async (req, res) => {
       $project: {
         email: 1,
         username: 1,
-        fullName: 1,
+        fullname: 1,
         subscribersCount: 1,
         channelsSubscribedToCount: 1,
         isSubscribed: 1,
@@ -506,7 +506,59 @@ const getUserChannelProfile = AsyncFnHandler(async (req, res) => {
     );
 });
 
-const getWatchHistory = AsyncFnHandler(async (req, res) => {});
+const getWatchHistory = AsyncFnHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    );
+});
 
 export {
   registerUser,
